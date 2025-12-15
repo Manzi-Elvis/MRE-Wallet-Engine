7fca46b34929d9eb08460a5e273dcff0e0d3ad27
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-15 10:39:42 +0200

    Wallet Entity

--- src/main/java/com/mre/wallet_engine/model/Wallet.java
+++ src/main/java/com/mre/wallet_engine/model/Wallet.java
@@ -1,5 +1,22 @@
 package com.mre.wallet_engine.model;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "wallets")
 public class Wallet {
-      
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    // Constructors, getters, setters
 }
