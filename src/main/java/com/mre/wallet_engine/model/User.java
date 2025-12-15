677b2675fca9fc39615ce42fc3638d68b6b6f9d8
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-15 10:39:20 +0200

    User Entity

---
+++ src/main/java/com/mre/wallet_engine/model/User.java
@@ -0,0 +1,24 @@
+package com.mre.wallet_engine.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Wallet wallet;
+
+    // Constructors, getters, setters
+}
