155e47492b17522e676da0dffb9da2572dfd4bfd
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-15 10:40:51 +0200

    User Repository

--- src/main/java/com/mre/wallet_engine/repository/UserRepository.java
+++ src/main/java/com/mre/wallet_engine/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.mre.wallet_engine.repository;
 
-public class UserRepository {
-      
+import com.mre.wallet_engine.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
