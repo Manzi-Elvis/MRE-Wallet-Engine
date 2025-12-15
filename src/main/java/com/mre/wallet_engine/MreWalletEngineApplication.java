b7166aa6d24d8e11a75e737970942d6654cd6aa7
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-12-15 10:11:25 +0200

    ...

--- src/main/java/com/mre/wallet_engine/MreWalletEngineApplication.java
+++ src/main/java/com/mre/wallet_engine/MreWalletEngineApplication.java
@@ -2,15 +2,8 @@ package com.mre.wallet_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-    exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-    }
-)
+@SpringBootApplication
 public class MreWalletEngineApplication {
 
     public static void main(String[] args) {
